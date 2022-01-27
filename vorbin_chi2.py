139a7ea7d85455d93ef2e0e3be46261659e5f6ff
200k33p3r <martin.ying.gr@dartmouth.edu>
2022-01-27 14:42:22 -0500

    Add files via upload

--- vorbin_chi2.py
+++ vorbin_chi2.py
@@ -9,11 +9,11 @@ class chi2:
 
     def read_input(self):
         #read vorbin file
-        dp = pd.read_csv("C:/Users/marti/Desktop/share/vorbin_chi2/sn={}".format(self.sn))
+        dp = pd.read_csv("/home/mying/Desktop/vorbin_chi2/sn={}".format(self.sn))
         self.xBar = dp['xBar'].values
         self.yBar = dp['yBar'].values
         #read observed bin counts
-        dp = pd.read_csv("C:/Users/marti/Desktop/share/vorbin_chi2/bin_count_std_sn={}".format(self.sn))
+        dp = pd.read_csv("/home/mying/Desktop/vorbin_chi2/bin_count_std_sn={}".format(self.sn))
         self.bin_count_std = dp['bin_count_std'].values
         self.obs_size = np.sum(self.bin_count_std)
 
@@ -28,13 +28,13 @@ class chi2:
     def search_vorbin(self):
         #go through the search process
         Tb_size = 1000
-        dms = np.linspace(14.62,14.82,1)
-        reds = np.linspace(0.0,0.05,1)
+        dms = np.linspace(14.62,14.82,21)
+        reds = np.linspace(0.0,0.05,6)
         ages = ['09000','10000','11000','12000','13000','14000','15000','16000']
         chi2 = []
         for age in ages:
             #read iso files
-            dp = pd.read_csv("C:/Users/marti/Desktop/share/dm_test/mc{}.a{}".format(self.mc_num,age),sep='\s+',names=['vi','v','i'],skiprows=3)
+            dp = pd.read_csv("/data/outcmd/mc{}.a{}".format(self.mc_num,age),sep='\s+',names=['vi','v','i'],skiprows=3)
             for dm in dms:
                 for red in reds:
                     bin_count = np.zeros(len(self.xBar))
@@ -48,13 +48,16 @@ class chi2:
                     bin_num = self.search_point_location_bc((dp['vi'].values[n_div*Tb_size:] + red)*12.5, dp['v'].values[n_div*Tb_size:] + dm, self.xBar, self.yBar)
                     for j in range(total_pt - n_div*Tb_size):
                         bin_count[bin_num[j]] += 1
-                    chi2.append([age, dm, red, np.inner(np.divide(bin_count,self.bin_count_std*(total_pt/self.obs_size)) - 1, bin_count - self.bin_count_std*(total_pt/self.obs_size))])
+                    #to avoid divde by 0
+                    for i in range(len(bin_count)):
+                        if bin_count[i] == 0:
+                            bin_count[i] += 1
+                    chi2.append([age, dm, red, np.inner(np.divide(self.bin_count_std,bin_count/(total_pt/self.obs_size)) - 1, self.bin_count_std - bin_count/(total_pt/self.obs_size))])
         self.chi2 = chi2
     def writeout(self):
         #write chi2 to csv file
         dp = pd.DataFrame(data=self.chi2,columns=['age','dm','red','chi2'])
-        cwd = os.getcwd()
-        path = cwd + "/outchi2/chi2_mc{}_sn{}".format(self.mc_num,self.sn)
+        path = "/data/outchi2/chi2_mc{}_sn{}".format(self.mc_num,self.sn)
         dp.to_csv(path)
 
     def __init__(self, mc_num, sn):
